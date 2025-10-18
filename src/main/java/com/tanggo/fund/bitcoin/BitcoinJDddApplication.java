b26c77c878ff22948db793668c4d5e47f51289e4
thy3368 <thy_email@126.com>
2025-10-18 16:41:23 +0800

    bitcoin ddd版本，1）为了清晰的了解bitcoin工作原理 2）让bitcoin有更加清晰健壮的代码。

--- src/main/java/com/tanggo/fund/bitcoin/BitcoinJDddApplication.java
+++ src/main/java/com/tanggo/fund/bitcoin/BitcoinJDddApplication.java
@@ -2,8 +2,13 @@ package com.tanggo.fund.bitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.tanggo.fund.bitcoin",
+        "com.tanggo.fund.eth"
+})
 public class BitcoinJDddApplication {
 
     public static void main(String[] args) {
