a0f17197247c614bf716fd645182e3829a8392c3
thy3368 <thy_email@126.com>
2025-10-20 22:33:57 +0800

    bitcoin ddd版本，1）为了清晰的了解bitcoin工作原理 2）让bitcoin有更加清晰健壮的代码。

--- src/main/java/com/tanggo/fund/raft/domain/LogEntry.java
+++ src/main/java/com/tanggo/fund/raft/domain/LogEntry.java
@@ -1,13 +1,18 @@
 package com.tanggo.fund.raft.domain;
 
+import lombok.Data;
+
 // 命令行流水 binlog
+@Data
 public class LogEntry {
+    private final String nodeId;
     private final int term;        // 创建时的任期号
     private final int index;       // 日志索引位置
     private final String command;  // 操作指令
     private boolean committed; // 是否已提交
 
-    public LogEntry(int term, int index, String command) {
+    public LogEntry(String nodeId, int term, int index, String command) {
+        this.nodeId = nodeId;
         this.term = term;
         this.index = index;
         this.command = command;
--- src/main/java/com/tanggo/fund/raft/domain/PeerRaftNode.java
+++ src/main/java/com/tanggo/fund/raft/domain/PeerRaftNode.java
@@ -1,6 +1,13 @@
 package com.tanggo.fund.raft.domain;
 
+import lombok.Data;
+
+@Data
 public class PeerRaftNode {
+
+    private String nodeId;
+    private String remoteNodeId;
+
 
     //远程信息
     //node id等
